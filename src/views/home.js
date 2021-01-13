e42f99021026707407230d0be2fab1d4a709161b
shellymutu-grigg <shellymutugrigg@gmail.com>
2021-01-13 23:22:35 +1300

    implement Auth0 login/logout via hooks with landing page

--- src/views/home.js
+++ src/views/home.js
@@ -3,8 +3,14 @@ import React from "react";
 import AuthenticationButton from "../components/authentication-button";
 
 const Home = () => (
-  
-    <AuthenticationButton />
+  <>
+    <div className='box'>
+      <h1 className='cardTitle'>Auth0 Demo</h1>
+        <h2 className='landingText'>Please select login / logout to use Auth0</h2>
+        <br></br>
+      <AuthenticationButton />
+    </div>
+  </>
 )
 
 export default Home;
